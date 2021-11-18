36188882b04ed25a43133d9b3aa8426eced0009f
emanuel2718 <eramirez2718@gmail.com>
2021-11-18 13:49:30 -0400

    game of life is working

--- gameoflife.cpp
+++ gameoflife.cpp
@@ -7,24 +7,22 @@
 #include "util.hpp"
 
 
-
 void GameOfLife::simulate(std::string map) {
     set_map(map);
     reset_round();
     populate_board();
-    //nodelay(stdscr, TRUE);
 
     int ascii_code;
-    bool running = true, delay = true;
+    bool running = true;
     while (running) {
         clear();
         render_simulation();
         refresh();
+        ascii_code = getch();
         update_board();
         refresh();
-        ascii_code = getch();
 
-        if (!delay) timeout(500);
+        if (delay_on) timeout(100);
         switch (ascii_code) {
             case 27: case 113: // ESC
                 running = false;
@@ -35,17 +33,8 @@ void GameOfLife::simulate(std::string map) {
                 running = false;
                 run();
                 break;
-            case 114:
-                break;
             case 112: // 'p' for pause
-                if (delay) {
-                    delay = false;
-                    set_delay(false);
-                }
-                else {
-                    delay = true;
-                    set_delay(true);
-                }
+                toogle_delay();
                 break;
             default:
                 break;
@@ -53,11 +42,15 @@ void GameOfLife::simulate(std::string map) {
     }
 }
 
-void GameOfLife::set_delay(bool delay) {
-    if (delay)
+void GameOfLife::toogle_delay() {
+    if (delay_on) {
+        set_delay(false);
         nodelay(stdscr, FALSE);
-    else
+    }
+    else {
+        set_delay(true);
         nodelay(stdscr, TRUE);
+    }
 }
 
 void GameOfLife::render_simulation() {
@@ -68,9 +61,14 @@ void GameOfLife::render_simulation() {
     refresh();
     for (size_t i = 0; i < board.size(); ++i) {
         for (size_t j = 0; j < board[0].size(); ++j) {
-            mvaddch(i+get_height()/2 - board.size()/2,
-                    j+get_width()/2 - board[0].size()/2,
-                    board[i][j]);
+            if (board[i][j] == LIVE_CELL)
+                mvaddch(i+get_height()/2 - board.size()/2,
+                        j+get_width()/2 - board[0].size()/2,
+                        RENDER_LIVE_CELL);
+            else
+                mvaddch(i+get_height()/2 - board.size()/2,
+                        j+get_width()/2 - board[0].size()/2,
+                        RENDER_DEAD_CELL);
         }
     }
 }
@@ -78,15 +76,12 @@ void GameOfLife::render_simulation() {
 
 void GameOfLife::populate_board() {
     std::ifstream file;
-    std::ofstream debug_log("debug.txt");
     file.open(map);
     if (!file) return;
     for (int i = 0; i < ROWS; ++i) {
         for (int j = 0; j < COLUMNS; ++j) {
             file >> board[i][j];
-            debug_log << board[i][j];
         }
-        debug_log << '\n';
     }
     return;
 }
@@ -106,162 +101,60 @@ void GameOfLife::populate_board() {
        Similarly, all other dead cells stay dead.
 
 */
-    //0  1  2  3
-// [0, 0, 0, 0] size = 4, size-1 = 3
 void GameOfLife::update_board() {
-
     // init temp board
     std::vector<std::vector<char>> temp_board(ROWS, std::vector<char>(COLUMNS, '.'));
 
-    int alive_count = 0; // number of neighbors alive
-    for (size_t i = 0; i < board.size(); ++i) {
-        for (size_t j = 0; j < board[0].size(); ++j) {
+    int alive_count;
+    for (size_t y = 0; y < board.size(); ++y) {
+        for (size_t x = 0; x < board[0].size(); ++x) {
             alive_count = 0;
-
             // top left
-            if (i != 0 && j != 0 && board[i-1][j-1] == LIVE_CELL)
-                alive_count++;
+            if (x != 0 && y != 0 && board[y-1][x-1] == LIVE_CELL) alive_count++;
 
             // top
-            if (i != 0 && board[i-1][j] == LIVE_CELL)
-                alive_count++;
+            if (y != 0 && board[y-1][x] == LIVE_CELL) alive_count++;
 
             // top right
-            if (i != 0 && j < board[0].size()-1 && board[i-1][j+1] == LIVE_CELL)
-                alive_count++;
+            if (y != 0 && board[y-1][x+1] == LIVE_CELL) alive_count++;
 
             // left
-            if (j != 0 && board[i][j-1] == LIVE_CELL)
-                alive_count++;
+            if (x != 0 && board[y][x-1] == LIVE_CELL) alive_count++;
 
             // right
-            if (j < board[0].size()-1 && board[i][j+1])
-                alive_count++;
+            if (x != board[0].size()-1 && board[y][x+1] == LIVE_CELL) alive_count++;
 
             // bottom left
-            if (i < board.size()-1 && j != 0 && board[i+1][j-1] == LIVE_CELL)
-                alive_count++;
+            if (x != 0 && y != board.size()-1 && board[y+1][x-1] == LIVE_CELL) alive_count++;
 
             // bottom
-            if (i < board.size()-1 && board[i+1][j] == LIVE_CELL)
-                alive_count++;
+            if (y != board.size()-1 && board[y+1][x] == LIVE_CELL) alive_count++;
 
             // bottom right
-            if (i < board.size()-1 && j < board[0].size()-1 && board[i+1][j+1] == LIVE_CELL)
-                alive_count++;
-
-
-            /*
-                1. Any LIVE cell with fewer than two live neighbours DIES
-                2. Any LIVE cell with two or three live neighbours LIVES
-                3. Any LIVE cell with more than three live neighbours DIES
-                4. Any DEAD cell with exactly three live neighbours becomes a LIVE cell
-            */
+            if (y != board.size()-1 && x != board[0].size()-1 && board[y+1][x+1] == LIVE_CELL) alive_count++;
 
             // RULE 1
-            if (board[i][j] == LIVE_CELL && alive_count < 2)
-                temp_board[i][j] = DEAD_CELL;
+            if (board[y][x] == LIVE_CELL && alive_count < 2)
+                temp_board[y][x] = DEAD_CELL;
 
             // RULE 2
-            //else if (board[i][j] == LIVE_CELL && alive_count == 2)
-            //    board[i][j] = LIVE_CELL;
-
-            //else if (board[i][j] == LIVE_CELL && alive_count == 3)
-            //    board[i][j] = LIVE_CELL;
+            else if (board[y][x] == LIVE_CELL && (alive_count == 2 || alive_count == 3))
+                temp_board[y][x] = LIVE_CELL;
 
             // RULE 3
-            else if (board[i][j] == LIVE_CELL && alive_count > 3)
-                temp_board[i][j] = DEAD_CELL;
+            else if (board[y][x] == LIVE_CELL && alive_count > 3)
+                temp_board[y][x] = DEAD_CELL;
 
             // RULE 4
-            else if (board[i][j] == DEAD_CELL && alive_count == 3)
-                temp_board[i][j] = LIVE_CELL;
-
-
-            //if (board[i][j] == LIVE_CELL && alive_count < 2)
-            //    board[i][j] = DEAD_CELL;
-
-            //else if (board[i][j] == DEAD_CELL && (alive_count == 2 || alive_count == 3))
-            //    board[i][j] = LIVE_CELL;
-
-            //else if (board[i][j] == LIVE_CELL && alive_count > 3)
-            //    board[i][j] = DEAD_CELL;
-
-            //else if (board[i][j] == DEAD_CELL && alive_count == 3)
-            //    board[i][j] = LIVE_CELL;
+            else if (board[y][x] == DEAD_CELL && alive_count == 3)
+                temp_board[y][x] = LIVE_CELL;
         }
     }
-
     // Fill the the actual board
-    for (size_t i = 0; i < temp_board.size(); ++i)
-        for (size_t j = 0; j < temp_board[0].size(); ++j)
-            board[i][j] = temp_board[i][j];
+    for (size_t y = 0; y < temp_board.size(); ++y)
+        for (size_t x = 0; x < temp_board[0].size(); ++x)
+            board[y][x] = temp_board[y][x];
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//void GameOfLife::update_board() {
-//    for (size_t y = 0; y < board.size(); ++y) {
-//        for (size_t x = 0; x < board[0].size(); ++x) {
-//            int alive_count = 0;
-//            // top left
-//            if (x != 0 && y != 0 && board[y-1][x-1] == LIVE_CELL) alive_count++;
-//
-//            // top
-//            if (y != 0 && board[y-1][x] == LIVE_CELL) alive_count++;
-//
-//            // top right
-//            if (y != 0 && board[y-1][x+1] == LIVE_CELL) alive_count++;
-//
-//            // left
-//            if (x != 0 && board[y][x-1] == LIVE_CELL) alive_count++;
-//
-//            // right
-//            if (x != board[0].size()-1 && board[y][x+1] == LIVE_CELL) alive_count++;
-//
-//            // bottom left
-//            if (x != 0 && y != board.size()-1 && board[y+1][x-1] == LIVE_CELL) alive_count++;
-//
-//            // bottom
-//            if (y != board.size()-1 && board[y+1][x] == LIVE_CELL) alive_count++;
-//
-//            // bottom right
-//            if (y != board.size()-1 && x != board[0].size()-1 && board[y+1][x+1] == LIVE_CELL) alive_count++;
-//
-//            if (board[y][x] == LIVE_CELL && alive_count < 2)
-//                board[y][x] = DEAD_CELL;
-//            else if (board[y][x] == LIVE_CELL && (alive_count == 2 || alive_count == 3))
-//                board[y][x] = LIVE_CELL;
-//            else if (board[y][x] == LIVE_CELL && alive_count > 3)
-//                board[y][x] = DEAD_CELL;
-//            else if (board[y][x] == DEAD_CELL && alive_count == 3)
-//                board[y][x] = LIVE_CELL;
-//
-//            // Rule 1: Any live cell with two or three live neighbours survives.
-//            //if (board[y][x] == LIVE_CELL && (alive_count == 2 || alive_count == 3)) {
-//            //    board[y][x] = LIVE_CELL;
-//            //}
-//
-//            //// Rule 2: Any dead cell with three live neighbours becomes a live cell.
-//            //else if (board[y][x] == DEAD_CELL && alive_count == 3)
-//            //    board[y][x] = LIVE_CELL;
-//        }
-//    }
-//
-//
-//
-//}
 
 void GameOfLife::run() {
     clear();
--- gameoflife.hpp
+++ gameoflife.hpp
@@ -22,15 +22,30 @@ class GameOfLife {
         void update_board();
         void populate_board();
         void render_simulation();
-        void set_delay(bool delay_on);
+        void toogle_delay();
+
+        // NOTE: for the reset I need to grab the map code. Because
+        //       if we try to reset the map using the current map is going to be
+        //       already changed
+        //void reset_map(std::string map);
+        bool is_delay() { return delay_on; }
+        void set_delay(bool delay) { delay_on = delay; }
         void run();
 
     private:
+        // the board only knows about these two chars
         static const char LIVE_CELL = 'X';
         static const char DEAD_CELL = '.';
+
+        // if the cell is a LIVE_CELL it will render as RENDER_LIVE_CELL in the screen
+        static const char RENDER_LIVE_CELL = 'X';
+        // if the cell is a DEAD_CELL it will render as RENDER_DEAD_CELL in the screen
+        static const char RENDER_DEAD_CELL = ' ';
+
         std::string map;
         std::vector<std::vector<char>> board;
         int round = 0;
+        bool delay_on = false; // basically control run or pause. Might change the name.
 };
 
 
